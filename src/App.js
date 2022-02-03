e29888a7dbac559c1c4a443c07ab6ebb9bf8c0c1
unknown <hdamitzian@gmail.com>
2022-02-03 14:41:01 +0100

    first commit client

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { useState } from "react";
+import { Button, Container, Stack } from "react-bootstrap";
+import AddPostModal from "./components/AddPostModal";
 function App() {
+  const [showAddPostModal, setShowAddPostModal] = useState(false);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Container className="mx-2">
+        <Stack direction="horizontal" gap={2}>
+          <h1 className="me-auto">What?</h1>
+        <Button variant="primary" onClick={() => setShowAddPostModal(true)}>Ajouter un post</Button>
+        </Stack>
+      </Container>  
+      <AddPostModal show={showAddPostModal} handleClose={() => setShowAddPostModal(false)}/>
+    </>
   );
 }
 
---
+++ src/components/AddPostModal.js
@@ -0,0 +1,35 @@
+import React, { useRef } from 'react';
+import { Button, Form, Modal } from 'react-bootstrap';
+export default function AddPostModal({show, handleClose}) {
+  const titleRef = useRef();
+  const messageRef = useRef();
+
+  function handleSubmit(e){
+    e.preventDefault();
+    console.log("submit!!");
+    handleClose()
+  }
+  
+  return (
+    <Modal show={show} onHide={handleClose}>
+      <Form onSubmit={handleSubmit}>
+        <Modal.Header closeButton>
+          <Modal.Title>Publier un truc!</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Form.Group>
+            <Form.Label>Titre</Form.Label>
+            <Form.Control type='text' required ref={titleRef}/>
+          </Form.Group>
+          <Form.Group>
+            <Form.Label>Message</Form.Label>
+            <Form.Control type='text' ref={messageRef} required/>
+          </Form.Group>
+          <div>
+            <Button type='submit' className='mt-2'>Publier!</Button>
+          </div>
+        </Modal.Body>
+      </Form>
+    </Modal>
+  );
+}
